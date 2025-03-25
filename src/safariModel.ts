52734827c82d07a6c0f29305df37251cbb9fa206
Menyhárt Attila <x5tgh1@inf.elte.hu>
2025-03-25 23:24:14 +0100

    ✨ feat: SafariModel base class

--- src/safariModel.ts
+++ src/safariModel.ts
@@ -7,14 +7,28 @@ export default class SafariModel {
     this._map = new Map()
   }
 
-  public tick = () => {}
+  get width(): number {
+    return this._map.width
+  }
+
+  get height(): number {
+    return this._map.height
+  }
+
+  public tick = () => {
+    this._map.tick()
+  }
 
   public getAllDrawData = (): DrawData[] => {
     return this._map.getAllDrawData()
   }
 }
 
+// Temporary Map class before implementing the actual map logic
 class Map {
+  get width(): number { return 0 }
+  get height(): number { return 0 }
+  public tick = () => {}
   public getAllDrawData = (): DrawData[] => {
     return []
   }
