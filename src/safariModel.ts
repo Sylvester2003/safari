d12c3d74901ab984c1396bf301ddf520d9191571
Menyhárt Attila <x5tgh1@inf.elte.hu>
2025-03-30 22:24:41 +0200

    📝 docs: SafariModel

--- src/safariModel.ts
+++ src/safariModel.ts
@@ -2,34 +2,77 @@ import type DrawData from '@/drawData'
 import Map from '@/map'
 import { createTile } from './utils/registry'
 
+/**
+ * Overarching model class for managing the game state and logic.
+ */
 export default class SafariModel {
   private readonly _map: Map
 
+  /**
+   * Creates an instance of the SafariModel class.
+   */
   constructor() {
     this._map = new Map(48, 27)
   }
 
+  /**
+   * Loads the map.
+   *
+   * @returns A promise that resolves when the map is loaded.
+   */
   public loadMap = async (): Promise<void> => {
     await this._map.loadMap()
   }
 
+  /**
+   * Gets the width of the map.
+   *
+   * @returns The width of the map.
+   */
   public get width(): number {
     return this._map.width
   }
 
+  /**
+   * Gets the height of the map.
+   *
+   * @returns The height of the map.
+   */
   public get height(): number {
     return this._map.height
   }
 
+  /**
+   * Updates the game state by one tick.
+   */
   public tick = () => {
     this._map.tick()
   }
 
+  /**
+   * Gets the draw data for all the tiles on the map.
+   *
+   * @returns An array of draw data for all the tiles on the map.
+   */
   public getAllDrawData = (): DrawData[] => {
     return this._map.getAllDrawData(false) // TODO: isNight
   }
 
-  public buyTile = async (tileId: string, x: number, y: number) => {
+  /**
+   * Places a tile on the map at the specified coordinates.
+   *
+   * WIP: Implementation for updating the balance of the player and checking if the tile can be placed.
+   *
+   * @param tileId - The ID of the tile to be placed.
+   * @param x - The x grid position where the tile should be placed.
+   * @param y - The y grid position where the tile should be placed.
+   * @returns A promise that resolves when the tile is placed.
+   */
+  public buyTile = async (
+    tileId: string,
+    x: number,
+    y: number,
+  ): Promise<void> => {
     const tile = createTile(tileId, x, y)
     if (tile) {
       await tile.loadDrawData()
