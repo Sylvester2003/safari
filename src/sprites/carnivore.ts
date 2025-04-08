7f648b4f37a718f46b34a16771c6167775ad44ed
Csepi Szilveszter <u0zamr@inf.elte.hu>
2025-04-08 18:48:22 +0200

    ✨ feat: carnivore class

---
+++ src/sprites/carnivore.ts
@@ -0,0 +1,23 @@
+import type Herbivore from '@/sprites/herbivore'
+import type Tile from '@/tiles/tile'
+import Animal from '@/sprites/animal'
+
+export default class Carnivore extends Animal {
+  private _chasing?: Herbivore
+
+  constructor(x: number, y: number, group: number) {
+    super(x, y, group)
+  }
+
+  public act(_dt: number, _visibleSprites: Animal[], _visibleTiles: Tile[]): void {
+
+  }
+
+  public isEnganged(): boolean {
+    return false
+  }
+
+  public toString() {
+    return 'safari:carnivore'
+  }
+}
