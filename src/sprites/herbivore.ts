669c5bcb7fb9c2058e2f84b7adb4bf5b12294a8e
Csepi Szilveszter <u0zamr@inf.elte.hu>
2025-04-22 03:16:08 +0200

    ✨ feat: herbivore food memory

--- src/sprites/herbivore.ts
+++ src/sprites/herbivore.ts
@@ -1,3 +1,4 @@
+import type Tile from '@/tiles/tile'
 // import type Carnivore from '@/sprites/carnivore'
 import Animal from '@/sprites/animal'
 
@@ -25,5 +26,36 @@ export default abstract class Herbivore extends Animal {
    */
   public isEnganged = (): boolean => {
     return false
+  }
+
+  protected updateFoodMemory = (tiles: Tile[]): void => {
+    const nearEdiblePositions = new Set<string>()
+
+    tiles.forEach((tile) => {
+      const key = tile.position.toString()
+
+      if (tile.isEdible) {
+        this._seenFoodPositions.add(tile.position)
+        nearEdiblePositions.add(key)
+      }
+    })
+
+    // Törlés a _seenFoodPositions-ből, ha már nem szerepel ehetőként
+    for (const pos of this._seenFoodPositions) {
+      const key = pos.toString()
+      const isNear = tiles.some(tile => tile.position[0] === pos[0] && tile.position[1] === pos[1])
+      if (!nearEdiblePositions.has(key) && isNear) {
+        this._seenFoodPositions.delete(pos)
+      }
+    }
+  }
+
+  protected fillFoodLevel = (visibleTiles: Tile[]): void => {
+    const currentTile = this.getTileByPosition(visibleTiles, this.position[0], this.position[1])
+
+    if (currentTile?.isEdible) {
+      this._foodLevel = 100
+      // this._restingTime = 25 + Math.random() * 20
+    }
   }
 }
