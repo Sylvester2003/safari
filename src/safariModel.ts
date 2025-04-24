ce9ec264c4a48f55f36c49904640248843d65f30
Tóth Henrik <irw5qp@inf.elte.hu>
2025-04-24 02:09:55 +0200

    fix: fixed daysGoalsMet updating too many times

--- src/safariModel.ts
+++ src/safariModel.ts
@@ -28,6 +28,8 @@ export default class SafariModel {
   private _isOpen: boolean
   private _time: number
   private _timer: number
+  private _daysGoalMet: number
+  private _lastGoalCheckDay: number
 
   /**
    * Gets the goal of the game.
@@ -177,6 +179,8 @@ export default class SafariModel {
     this._isOpen = false
     this._timer = 0
     this._time = 0
+    this._daysGoalMet = 0
+    this._lastGoalCheckDay = -1
   }
 
   /**
@@ -206,14 +210,31 @@ export default class SafariModel {
           this._map.queueVisitor(visitor)
         }
       }
-     
+      this.checkGoalsMet()
+    }
+  }
+
+  /**
+   * Checks if a day has passed and if all goals are met.
+   * Emits the goalMetSignal if conditions are satisfied.
+   */
+  public checkGoalsMet = () => {
+    const currentDay = Math.floor(this._time / 1440)
+    if (this._time >= 1440 && this._lastGoalCheckDay !== currentDay) {
       if (
         this.balance >= this.goal.balance &&
         this._map.getHerbivoreCount() >= this.goal.herbivores &&
-        this._map.getCarnivoreCount() >= this.goal.carnivores 
+        this._map.getCarnivoreCount() >= this.goal.carnivores
       ) {
-        goalMetSignal.emit()
+        this._daysGoalMet++
+        if (this._daysGoalMet >= this.goal.forDays) {
+          goalMetSignal.emit()
+        }
       }
+      else {
+        this._daysGoalMet = 0
+      }
+      this._lastGoalCheckDay = currentDay
     }
   }
 
