5853af484a6d85505a4b85b14eea4a5bbdbbf3ce
Csepi Szilveszter <u0zamr@inf.elte.hu>
2025-05-17 21:09:23 +0200

    ✨ feat: poacher class

--- src/sprites/shooter.ts
+++ src/sprites/shooter.ts
@@ -2,17 +2,17 @@ import Sprite from '@/sprites/sprite'
 
 export default abstract class Shooter extends Sprite implements Shootable {
   protected _shootingAt: Shootable | null
-  private _bulletTimer: number
+  protected _bulletTimer: number
 
   constructor(x: number, y: number) {
     super(x, y)
     this._shootingAt = null
-    this._bulletTimer = 0
+    this._bulletTimer = 1
   }
 
   public shootAt = (sprite: Shootable) => {
     this._shootingAt = sprite
   }
 
-  public abstract getShotBy :(shooter: Shooter) => boolean
+  public abstract getShotBy: (shooter: Shooter) => boolean
 }
