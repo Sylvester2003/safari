aa0d61c4a1e54b3c02e821171271508f97fbaaca
Tóth Henrik <irw5qp@inf.elte.hu>
2025-05-18 14:53:11 +0200

    feat: implemented random movement for ranger

--- src/sprites/ranger.ts
+++ src/sprites/ranger.ts
@@ -1,3 +1,4 @@
+import { updateVisiblesSignal } from '@/utils/signal'
 import Shooter from './shooter'
 
 export default class Ranger extends Shooter {
@@ -25,7 +26,27 @@ export default class Ranger extends Shooter {
     return false
   }
 
-  public act = (_dt: number) => {
+  public act = (dt: number) => {
+    updateVisiblesSignal.emit(this)
+    this.updateState()
 
+    // If we're at our destination or don't have one, choose a new random target
+    if (this.isAtDestination() || !this.pathTo) {
+      const bounds = this.computeBounds(this._visibleTiles)
+      const nonObstacleTiles = this._visibleTiles.filter(tile => !tile.isObstacle)
+
+      if (nonObstacleTiles.length > 0) {
+        const randomTileIndex = Math.floor(Math.random() * nonObstacleTiles.length)
+        const randomTile = nonObstacleTiles[randomTileIndex]
+        this.pathTo = [
+          Math.max(bounds.minX, Math.min(bounds.maxX, randomTile.position[0])),
+          Math.max(bounds.minY, Math.min(bounds.maxY, randomTile.position[1])),
+        ]
+      }
+    }
+
+    // Move towards the target
+    const bounds = this.computeBounds(this._visibleTiles)
+    this.move(dt, bounds.minX, bounds.minY, bounds.maxX, bounds.maxY)
   }
 }
