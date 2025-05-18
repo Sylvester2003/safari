5d76a825ce434b6a388f6adf50b20380495e9d74
Tóth Henrik <irw5qp@inf.elte.hu>
2025-05-18 13:12:51 +0200

    feat: created base of ranger class

--- src/sprites/ranger.ts
+++ src/sprites/ranger.ts
@@ -1,1 +1,27 @@
-export default class Ranger {}
+import Shooter from './shooter'
+
+export default class Ranger extends Shooter {
+  private _chasing: Shootable | null
+  declare protected _jsonData: RangerJson
+
+  constructor(x: number, y: number) {
+    super(x, y)
+    this._chasing = null
+  }
+
+  public setChasing = (chasing: Shootable) => {
+    this._chasing = chasing
+  }
+
+  public get buyPrice(): number {
+    return this._jsonData.buyPrice
+  }
+
+  public getShotBy = (_shooter: Shooter): boolean => {
+    return false
+  }
+
+  public act = (_dt: number) => {
+
+  }
+}
---
+++ src/types/rangerJson.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Represents the structure of the animal data JSONs.
+ */
+interface RangerJson extends SpriteJson {
+  buyPrice: number
+}
