742df9f627b40c1c46264ac988917311a4c185be
Csepi Szilveszter <u0zamr@inf.elte.hu>
2025-05-19 19:29:31 +0200

    🔧 fix: Lion class name fixed

--- src/sprites/lion.ts
+++ src/sprites/lion.ts
@@ -2,4 +2,4 @@ import Carnivore from '@/sprites/carnivore'
 import { carnivore } from '@/utils/registry'
 
 @carnivore('safari:lion')
-export default class Zebra extends Carnivore {}
+export default class Lion extends Carnivore {}
