1b2caba040cf00d42f7b04b3767be45f1b39fae1
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2026-02-22 02:00:40 +0100

    fix: eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -11,5 +11,7 @@ export default antfu({
     'coverage',
     'dist',
     '.npm',
+    'node_modules',
+    '.github',
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import SafariButton from '@/safariButton'
 import SafariView from '@/safariView'
 
 customElements.define('safari-button', SafariButton, { extends: 'button' })
-      customElements.define('safari-view', SafariView)
+customElements.define('safari-view', SafariView)
